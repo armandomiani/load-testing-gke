4955895230ad299d95e0c9dfd2189dad5ee69805
Armando Miani <armando.miani@gmail.com>
2019-05-28 18:17:49 +0200

    Exporting logs to GCS

--- locust/src/run.sh
+++ locust/src/run.sh
@@ -4,9 +4,14 @@ LOCUST="/usr/local/bin/locust"
 LOCUS_OPTS="-f /locust/tasks.py --host=$TARGET_HOST"
 LOCUST_MODE=${LOCUST_MODE:-standalone}
 
+
 if [[ "$LOCUST_MODE" = "master" ]]; then
-    # LOCUS_OPTS="$LOCUS_OPTS --master --no-web -c $CLIENTS -r $HATCH_RATE --run-time $RUN_TIME --expect-slaves $SLAVES_COUNT --csv results"
-    LOCUS_OPTS="$LOCUS_OPTS --master --expect-slaves $SLAVES_COUNT --csv /locust/stats/results"
+    LOG_FILENAME="/locust/stats/results_$RANDOM"
+    if [[ "$MASTER_MODE" = "CI" ]]; then
+        LOCUS_OPTS="$LOCUS_OPTS --master --no-web -c $CLIENTS -r $HATCH_RATE --run-time $RUN_TIME --expect-slaves $SLAVES_COUNT --csv $LOG_FILENAME"
+    elif [[ "$MASTER_MODE" = "WEB" ]]; then
+        LOCUS_OPTS="$LOCUS_OPTS --master --expect-slaves $SLAVES_COUNT --csv $LOG_FILENAME"
+    fi
 elif [[ "$LOCUST_MODE" = "worker" ]]; then
     LOCUS_OPTS="$LOCUS_OPTS --slave --master-host=$LOCUST_MASTER"
 fi
---
+++ locust/src/setup_gcsfuse.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ "$LOCUST_MODE" = "master" ]]; then
+    echo "Setting up GCS Fuse..."
+
+    apt update && apt install -y lsb-release
+    export GCSFUSE_REPO=gcsfuse-`lsb_release -c -s`
+    echo "deb http://packages.cloud.google.com/apt $GCSFUSE_REPO main" | tee /etc/apt/sources.list.d/gcsfuse.list
+    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    apt update && apt install -y gcsfuse
+
+    rm /locust/stats/*.* && gcsfuse $GCS_LOG_BUCKET /locust/stats
+
+    echo "Finished."
+elif [[ "$LOCUST_MODE" = "worker" ]]; then
+    echo "Slave does not require connection to GCS"
+fi
