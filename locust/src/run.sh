f64468c5752148156b7dceab21611e15e795c15a
Armando Miani <armando.miani@gmail.com>
2019-05-28 16:35:43 +0200

    Parametrize locust

--- locust/src/run.sh
+++ locust/src/run.sh
@@ -5,7 +5,8 @@ LOCUS_OPTS="-f /locust/tasks.py --host=$TARGET_HOST"
 LOCUST_MODE=${LOCUST_MODE:-standalone}
 
 if [[ "$LOCUST_MODE" = "master" ]]; then
-    LOCUS_OPTS="$LOCUS_OPTS --master"
+    # LOCUS_OPTS="$LOCUS_OPTS --master --no-web -c $CLIENTS -r $HATCH_RATE --run-time $RUN_TIME --expect-slaves $SLAVES_COUNT --csv results"
+    LOCUS_OPTS="$LOCUS_OPTS --master --expect-slaves $SLAVES_COUNT --csv /locust/stats/results"
 elif [[ "$LOCUST_MODE" = "worker" ]]; then
     LOCUS_OPTS="$LOCUS_OPTS --slave --master-host=$LOCUST_MASTER"
 fi
